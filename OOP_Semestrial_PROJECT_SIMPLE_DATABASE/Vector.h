0461e2f1a1512009cc6063d1bae2c1db0190d4d2
stefangpetrov <stefangpetrov@abv.bg>
2020-04-26 17:09:47 +0300

    Adding a missing file

---
+++ OOP_Semestrial_PROJECT_SIMPLE_DATABASE/Vector.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+template <class T>
+class Vector
+{
+private:
+    T* data;
+    size_t size;
+
+public:
+    Vector();
+    Vector(const Vector<T>& other);
+    template<class InitType>
+    Vector(size_t initial_size, const InitType& init);
+    Vector(size_t initial_size);
+    void to_end(const T& x);
+    void push(const T& x);
+    Vector<T> operator + (const Vector<T> other) const;
+    Vector<T> operator + (const T& x)  const;
+    Vector<T>& operator += (const T& x);
+    T& operator[] (size_t i);
+    T operator[] (size_t i) const;
+    Vector<T>& operator= (const Vector<T>& other);
+    ~Vector();
+    size_t length() const;
+    bool operator!= (const Vector<T>& other) const;
+    bool operator== (const Vector<T>& other) const;
+
+private:
+    void resize(size_t new_size);
+};
+
+template <class T>
+std::ostream& operator << (std::ostream& stream, const Vector<T>& v);
+
